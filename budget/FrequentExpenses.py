8f496296bf643e356f3730f38c70b653efd3e89b
Callum B <callumbuch10@msn.com>
2021-12-26 15:22:14 +0000

    5/6/7 counting top5 categories

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,5 @@
 from . import Expense
+import collections
 
 # Create new expenses object
 expenses = Expense.Expenses()
@@ -10,3 +11,10 @@ spending_categories=[]
 # Add catergories from csv to empty list
 for expense in expenses.list:
     spending_categories.append(expense.category)
+
+# Count the categories with the most purchases
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+# Split keys & values of top5 into separate lists
+categories, count = zip(*top5)
+
